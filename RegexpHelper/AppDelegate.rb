6a74c8f5a1ca9c2d7b057e60517435302ca70733
Gary Foster <gary.foster@gmail.com>
2011-07-23 08:30:19 -0700

    Initial Commit

---
+++ RegexpHelper/AppDelegate.rb
@@ -0,0 +1,15 @@
+#
+#  AppDelegate.rb
+#  RegexpHelper
+#
+#  Created by Gary Foster on 7/23/11.
+#  Copyright 2011 __MyCompanyName__. All rights reserved.
+#
+
+class AppDelegate
+    attr_accessor :window
+    def applicationDidFinishLaunching(a_notification)
+        # Insert code here to initialize your application
+    end
+end
+
